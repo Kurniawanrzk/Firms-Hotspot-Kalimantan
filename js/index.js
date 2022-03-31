c6a4b1251d6737e991d4aec741078a9b64ffeb66
KurniawanRizky <kurniawanrz205@gmail.com>
2022-03-31 07:39:44 +0700

    Update some file

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-function changeHref(){
-    var url = 'https://firms.modaps.eosdis.nasa.gov/api/area/csv/f82ecddd747a02cc05d8053cf515ad97/VIIRS_SNPP_NRT/110.76,-3.62,115.74,0.74/1/';
-    var a = document.createElement("a");
-    a.href = url;
-    fileName = url.split("/").pop();
-    a.download = fileName;
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
-    a.remove();
+function dari(){
+    var dari = document.getElementById('dari').value;
+    return dari;
+}
+function jarak(){
+var jarak = document.getElementById('jarak').value;
+return jarak;
 }
 
+function daerah(){
+    var daerah = document.getElementById('daerah').value;
+    return daerah;
+    }
 
+function ubahHref(){
+document.getElementById('firms').href = 'https://firms.modaps.eosdis.nasa.gov/api/area/csv/f82ecddd747a02cc05d8053cf515ad97/VIIRS_SNPP_NRT/'+daerah()+'/'+jarak()+'/'+dari()+'/';
+}
